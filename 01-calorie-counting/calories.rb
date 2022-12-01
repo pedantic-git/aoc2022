c72f59a75f52cd039346b7278691728f65f4365d
Quinn Daley <quinn@fishpercolator.co.uk>
2022-12-01 10:37:05 +0000

    And the solution to day 1 part 2

--- 01-calorie-counting/calories.rb
+++ 01-calorie-counting/calories.rb
@@ -4,5 +4,4 @@ elves = $<.readlines.chunk {|l| /\d/ =~ l || :_separator}
 calories = elves.map {|_, elf| elf.sum(&:to_i)}
 
 puts "Max calories: #{calories.max}"
-
-
+puts "Top three total calories: #{calories.max(3).sum}"
