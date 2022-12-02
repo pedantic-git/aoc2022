cd1cb6504f4fd11738314505c64529b3ed85012b
Quinn Daley <quinn@fishpercolator.co.uk>
2022-12-02 08:19:16 +0000

    Day 2 - part 1's solution

---
+++ 02-rock-paper-scissors/rps.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def score(round)
+  {'X' => 1, 'Y' => 2, 'Z' => 3}[round[1]] +
+  case round
+  when %w[A X], %w[B Y], %w[C Z]
+    3
+  when %w[A Y], %w[B Z], %w[C X]
+    6
+  else
+    0
+  end
+end
+
+rounds = $<.readlines.map(&:split)
+
+total = rounds.inject(0) {|acc,round| acc + score(round)}
+p total
