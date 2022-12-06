75055956c548f53113796c49ef9c8b59438cb6a8
Quinn Daley <quinn@fishpercolator.co.uk>
2022-12-06 09:38:25 +0000

    And make it work for part 2 by moving marker size to a constant

--- 06-tuning-trouble/marker-count.rb
+++ 06-tuning-trouble/marker-count.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# The size of the marker
+MS = 4 # or 14 for part 2
+
 # Not very efficient, but concise, way of finding the marker
-p $<.read.split('').each_cons(4).with_index.find {|c,i| c.uniq.size == 4}[1] + 4
+p $<.read.split('').each_cons(MS).with_index.find {|c,i| c.uniq.size == MS}[1] + MS
 
