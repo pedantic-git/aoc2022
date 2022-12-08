88549850ed3f6bad8ee7264f61e909df33fd471e
Quinn Daley <quinn@fishpercolator.co.uk>
2022-12-08 17:55:59 +0000

    And the solution to part 2

--- 08-treetop-tree-house/trees.rb
+++ 08-treetop-tree-house/trees.rb
@@ -20,7 +20,7 @@ def directions(trees, tree, index)
   west, east = ends(trees.row(row_number), col_number)
   north, south = ends(trees.column(col_number), row_number)
 
-  [north, east, west, south]
+  [north, east, south, west]
 end
 
 # Given a tree in trees and an index, return true if that tree is visible from
@@ -29,6 +29,22 @@ def visible?(trees, tree, index)
   directions(trees, tree, index).any? {|dir| dir.all? {|other| other < tree}}
 end
 
+# Given a tree in trees and an index, work out the scenic score
+def score(trees, tree, index)
+  dirs = directions(trees,tree,index)
+  # North and west are counting in the opposite direction (towards the tree)
+  # so reverse them
+  dirs[0].reverse! ; dirs[3].reverse!
+  dirs.map {|dir| until_block(dir, tree).length}.reduce(:*)
+end
+
+# Just the elements of this array that can be seen (up to the first one that is)
+# >= to tree
+def until_block(arr, tree)
+  arr.slice_after {|other| other>=tree}.first || []
+end
+
 trees = Matrix[*$<.readlines.map {_1.chomp.split('').map(&:to_i)}]
 
-p trees.each.with_index.count {|tree,index| visible?(trees, tree,index)}
+puts "Number of visible trees: #{trees.each.with_index.count {|tree,index| visible?(trees, tree,index)}}"
+puts "Largest scenic score: #{trees.each.with_index.map {|tree,index| score(trees, tree, index)}.max}"
